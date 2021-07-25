58d729816e0667c4334134e291bf7994a781ca28
Laetitia Studer <67235684+cosmoonl@users.noreply.github.com>
2021-07-25 18:51:25 +0100

    Add files via upload

---
+++ RecipesSearch.py
@@ -0,0 +1,46 @@
+import requests
+def recipe_search(ingredient, health):
+    app_id='9753f33f'
+    app_key='2691f4f2c2e5b89a09ebae1ce3a5529e'
+    url = 'https://api.edamam.com/api/recipes/v2?type=public&q={}&app_id={}&app_key={}&health={}'.format(ingredient, app_id, app_key, health)
+    result = requests.get(url)
+    data = result.json()
+    return data['hits']
+
+def run():
+    ingredient = input("Enter an ingredient: ")
+    health = input("What is your diet? ")
+    results = (recipe_search(ingredient, health))
+    recipes = []
+
+    for result in results:
+        recipes.append(result['recipe'])
+
+    recipes = sorted(recipes, key=lambda x: x['calories'])
+
+
+    for recipe in recipes:
+        print()
+        print(recipe['label'])
+        print(recipe['dietLabels'])
+        print("Number of portion: {}".format(int(recipe['yield'])))
+        print("This recipe contains {} calories.".format(int(recipe['calories'])))
+        Calorie_per_portion = int(recipe['calories'])/int(recipe['yield'])
+        print("Calories per portion: {:.0f}".format(Calorie_per_portion))
+        print("Suitable for: {}".format(recipe['healthLabels']))
+        print(recipe['image'])
+        print()
+
+    with open('recipes.txt', 'w+') as text_file:
+        for result in recipes:
+            text_file.write(str(result['label']) + '\n')
+            text_file.write(str(result['dietLabels']) + '\n')
+            text_file.write('{:.0f}'.format(result['yield']) + ' portions'+'\n')
+            text_file.write('{:.0f}'.format(result['calories']) + ' calories'+ '\n')
+            text_file.write(str(result['url']) + '\n')
+            text_file.write(str(result['healthLabels']) + '\n')
+            text_file.write('\n')
+
+run()
+
+
